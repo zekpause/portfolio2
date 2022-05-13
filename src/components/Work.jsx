9d9f81151d8cf0a13adb9ef7a9b2f2e574ce2330
zekpause <samson6603@gmail.com>
2022-05-13 17:47:52 +0800

    adding netflix-clone to work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import CrwnClothing from '../assets/crwn-clothing.png'
 import Face from '../assets/face.gif'
 import DragAndDrop from '../assets/drag-and-drop.gif';
-
+import Netflix from '../assets/netflix.png';
 
 const Work = () => {
     return (
@@ -65,6 +65,24 @@ const Work = () => {
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                                 </a>
                                 <a href="https://github.com/zekpause/drag-and-drop">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{ backgroundImage: `url(${Netflix})` }}
+                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                        {/* Hover Effect */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                React JS Application
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="https://netflix-react-5900b.web.app/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                </a>
+                                <a href="https://github.com/zekpause/netflix-clone">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                                 </a>
                             </div>
